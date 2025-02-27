7ecc8c5abed6e21f4b1fe62225250262dcfe30b3
OSIRIS <osiris.waongo@synelia.tech>
2025-02-27 22:40:44 +0000

    find user informations using UserDetailsService

--- src/main/java/com/osiris/banking/repository/UserRepository.java
+++ src/main/java/com/osiris/banking/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.osiris.banking.repository;
 import com.osiris.banking.dto.UserDto;
 import com.osiris.banking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
 
---
+++ src/main/java/com/osiris/banking/service/auth/UserServiceDetailsImpl.java
@@ -0,0 +1,22 @@
+package com.osiris.banking.service.auth;
+
+import com.osiris.banking.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceDetailsImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("No user was found with the provided email"));
+    }
+}
